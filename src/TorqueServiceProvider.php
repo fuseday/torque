fe9d59a3b8f2491b6ec35846855a9c69da850385
Joao Patricio <ijpatricio@gmail.com>
2021-07-10 20:02:15 +0100

    Cleans frontend files.

--- src/TorqueServiceProvider.php
+++ src/TorqueServiceProvider.php
@@ -2,28 +2,14 @@
 
 namespace Fuseday\Torque;
 
-use Fuseday\Torque\Commands\TorqueCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class TorqueServiceProvider extends PackageServiceProvider
+class TorqueServiceProvider extends ServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('torque')
-            ->hasMigration('create_torque_table')
-            ->hasRoutes('web')
-            ->hasCommand(TorqueCommand::class);
-
         $this->app->singleton('torque', Torque::class);
 
-        $this->loadViewsFrom($package->basePath('/../resources/views'), $package->name);
         $this->mergeConfigFrom(__DIR__.'/../config/torque.php', 'torque');
     }
 
